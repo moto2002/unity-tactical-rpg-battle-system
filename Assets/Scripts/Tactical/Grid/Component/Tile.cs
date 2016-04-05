0ebc19ac238ebc2b0724b769e57a00ea26806b50
Colin Bellino <bellino.colin@gmail.com>
2016-04-06 00:45:59 +0200

    feat(grid): create a new way to manage grids and cursors

---
+++ Assets/Scripts/Tactical/Grid/Component/Tile.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Tactical.Grid.Component {
+
+	using Model;
+
+	[ExecuteInEditMode]
+	public class Tile : MonoBehaviour {
+
+		public const float STEP_HEIGHT = 0.25f;
+
+		public Point pos;
+		public int height;
+		public Vector3 center {
+			get { return new Vector3(pos.x, height * STEP_HEIGHT, pos.y); }
+		}
+
+		public void Grow () {
+			height++;
+			Match();
+		}
+
+		public void Shrink () {
+			height--;
+			Match ();
+		}
+
+		public void Load (Point p, int h) {
+			pos = p;
+			height = h;
+			Match();
+		}
+
+		public void Load (Vector3 v) {
+			Load(new Point((int)v.x, (int)v.z), (int)v.y);
+		}
+
+		private void Update () {
+			Match();
+		}
+
+		private void Match () {
+			transform.localPosition = new Vector3(pos.x, height * STEP_HEIGHT / 2f, pos.y );
+			transform.localScale = new Vector3(1, height * STEP_HEIGHT, 1);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Tactical/Grid/Model/LevelData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Tactical.Grid.Model {
+
+	public class LevelData : ScriptableObject {
+		public List<Vector3> tiles;
+	}
+
+}
