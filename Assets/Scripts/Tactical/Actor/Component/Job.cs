4324960a3eefab2a21adb74dac9c174ca3348740
Colin Bellino <bellino.colin@gmail.com>
2016-04-27 23:50:39 +0200

    feat(actor): add job component and parser

---
+++ Assets/Scripts/Tactical/Actor/Component/Job.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Tactical.Core;
+using Tactical.Core.Component;
+using Tactical.Item.Component;
+using Tactical.Unit.Component;
+
+namespace Tactical.Actor.Component {
+
+	public class Job : MonoBehaviour {
+
+		public static readonly StatType[] statOrder = new StatType[] {
+			StatType.MHP,
+			StatType.MMP,
+			StatType.ATK,
+			StatType.DEF,
+			StatType.MAT,
+			StatType.MDF,
+			StatType.SPD
+		};
+
+		public int[] baseStats = new int[ statOrder.Length ];
+		public float[] growStats = new float[ statOrder.Length ];
+		private Stats stats;
+
+		private void OnDestroy () {
+			this.RemoveObserver(OnLvlChangeNotification, Stats.DidChangeNotification(StatType.LVL));
+		}
+
+		public void Employ () {
+			stats = gameObject.GetComponentInParent<Stats>();
+			this.AddObserver(OnLvlChangeNotification, Stats.DidChangeNotification(StatType.LVL), stats);
+
+			Feature[] features = GetComponentsInChildren<Feature>();
+			for (int i = 0; i < features.Length; ++i) {
+				features[i].Activate(gameObject);
+			}
+		}
+
+		public void UnEmploy () {
+			Feature[] features = GetComponentsInChildren<Feature>();
+			for (int i = 0; i < features.Length; ++i) {
+				features[i].Deactivate();
+			}
+
+			this.RemoveObserver(OnLvlChangeNotification, Stats.DidChangeNotification(StatType.LVL), stats);
+			stats = null;
+		}
+
+		public void LoadDefaultStats () {
+			for (int i = 0; i < statOrder.Length; ++i) {
+				StatType type = statOrder[i];
+				stats.SetValue(type, baseStats[i], false);
+			}
+
+			stats.SetValue(StatType.HP, stats[StatType.MHP], false);
+			stats.SetValue(StatType.MP, stats[StatType.MMP], false);
+		}
+
+		protected virtual void OnLvlChangeNotification (object sender, object args) {
+			int oldValue = (int)args;
+			int newValue = stats[StatType.LVL];
+
+			for (int i = oldValue; i < newValue; ++i) {
+				LevelUp();
+			}
+		}
+
+		private void LevelUp () {
+			for (int i = 0; i < statOrder.Length; ++i) {
+				StatType type = statOrder[i];
+				int whole = Mathf.FloorToInt(growStats[i]);
+				float fraction = growStats[i] - whole;
+
+				int value = stats[type];
+				value += whole;
+				if (UnityEngine.Random.value > (1f - fraction)) {
+					value++;
+				}
+
+				stats.SetValue(type, value, false);
+			}
+
+			stats.SetValue(StatType.HP, stats[StatType.MHP], false);
+			stats.SetValue(StatType.MP, stats[StatType.MMP], false);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Tactical/Actor/Editor/JobParser.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using Tactical.Core;
+using Tactical.Core.Component;
+using Tactical.Actor.Component;
+
+namespace Tactical.Actor.Editor {
+
+	public static class JobParser {
+
+		[MenuItem("Pre Production/Parse Jobs")]
+		public static void Parse() {
+			CreateDirectories();
+			ParseStartingStats();
+			ParseGrowthStats();
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+
+		private static void CreateDirectories () {
+			if (!AssetDatabase.IsValidFolder("Assets/Resources/Jobs")) {
+				AssetDatabase.CreateFolder("Assets/Resources", "Jobs");
+			}
+		}
+
+		private static void ParseStartingStats () {
+			string readPath = string.Format("{0}/Settings/JobStartingStats.csv", Application.dataPath);
+			string[] readText = File.ReadAllLines(readPath);
+			for (int i = 1; i < readText.Length; ++i) {
+				PartsStartingStats(readText[i]);
+			}
+		}
+
+		private static void PartsStartingStats (string line) {
+			string[] elements = line.Split(',');
+			GameObject obj = GetOrCreate(elements[0]);
+			Job job = obj.GetComponent<Job>();
+			for (int i = 1; i < Job.statOrder.Length + 1; ++i) {
+				job.baseStats[i - 1] = Convert.ToInt32(elements[i]);
+			}
+
+			StatModifierFeature move = GetFeature (obj, StatType.MOV);
+			move.amount = Convert.ToInt32(elements[8]);
+
+			StatModifierFeature jump = GetFeature (obj, StatType.JMP);
+			jump.amount = Convert.ToInt32(elements[9]);
+		}
+
+		private static void ParseGrowthStats () {
+			string readPath = string.Format("{0}/Settings/JobGrowthStats.csv", Application.dataPath);
+			string[] readText = File.ReadAllLines(readPath);
+			for (int i = 1; i < readText.Length; ++i) {
+				ParseGrowthStats(readText[i]);
+			}
+		}
+
+		private static void ParseGrowthStats (string line) {
+			string[] elements = line.Split(',');
+			GameObject obj = GetOrCreate(elements[0]);
+			Job job = obj.GetComponent<Job>();
+			for (int i = 1; i < elements.Length; ++i) {
+				job.growStats[i - 1] = Convert.ToSingle(elements[i]);
+			}
+		}
+
+		private static StatModifierFeature GetFeature (GameObject obj, StatType type) {
+			StatModifierFeature[] smf = obj.GetComponents<StatModifierFeature>();
+			for (int i = 0; i < smf.Length; ++i) {
+				if (smf[i].type == type) {
+					return smf[i];
+				}
+			}
+
+			StatModifierFeature feature = obj.AddComponent<StatModifierFeature>();
+			feature.type = type;
+			return feature;
+		}
+
+		private static GameObject GetOrCreate (string jobName) {
+			string fullPath = string.Format("Assets/Resources/Jobs/{0}.prefab", jobName);
+			GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(fullPath);
+			if (obj == null) {
+				obj = Create(fullPath);
+			}
+			return obj;
+		}
+
+		private static GameObject Create (string fullPath) {
+			GameObject instance = new GameObject ("temp");
+			instance.AddComponent<Job>();
+			GameObject prefab = PrefabUtility.CreatePrefab( fullPath, instance );
+			GameObject.DestroyImmediate(instance);
+			return prefab;
+		}
+
+	}
+
+}
